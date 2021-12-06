1c437414f6f6e09ae37e0b1ea0b81bc0c728c851
zmd9220 <zmd9220@gmail.com>
2021-12-06 14:19:08 +0900

    211206 [Feat] : 컨테이너에 등록된 모든 빈 조회

---
+++ core/src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,40 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            // 타입을 모르므로 오브젝트 타입
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = " + beanDefinitionName + " object = " + bean);
+//            System.out.println("ApplicationContextInfoTest.findAllBean");
+        }
+    }
+    @Test
+    @DisplayName("애플리케이션 빈 출력하기")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            // 각 bean 에 대한 메타데이터 정보
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+            // Role ROLE_APPLICATION: 직접 등록한 애플리케이션 빈
+            // Role ROLE_INFRASTRUCTURE: 스프링이 내부에서 사용하는 빈
+            // 스프링 내부에서 무언가 로직처리를 하기 위해 등록된 bean이 아니라 내가 앱을 개발하기 위해서 등록한 bean일 때 or 외부 라이브러리 사용할 때
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + " object = " + bean);
+            }
+        }
+    }
+}
